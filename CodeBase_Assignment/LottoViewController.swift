b630329b55db9a0c5ed95b517c788026033fa57f
rkdtnlzl <rkdtnlzl@naver.com>
2024-06-05 15:29:53 +0900

    [Fix] 프로젝트 이름 변경

---
+++ CodeBase_Assignment/LottoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LottoViewController.swift
+//  CodeBase_Assignment
+//
+//  Created by 강석호 on 6/5/24.
+//
+
+import UIKit
+
+class LottoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
